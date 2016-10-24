e70184326bea7b507bbd9816e71077f4be97b1fc
GaoMatrix <gcquan08@gmail.com>
2016-10-24 17:47:47 +0800

    添加TabLayout的三个Fragmnet结构

--- MaoYanApp/src/main/java/com/mao/movie/MainActivity.java
+++ MaoYanApp/src/main/java/com/mao/movie/MainActivity.java
@@ -1,13 +1,69 @@
 package com.mao.movie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.mao.movie.fragment.HotCommentFragment;
+import com.mao.movie.fragment.MainFragment;
+import com.mao.movie.fragment.MyFragment;
+import com.mao.movie.fragment.TabEntity;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.container)
+    FrameLayout mContainer;
+    @BindView(R.id.tabLayout)
+    CommonTabLayout mTabLayout;
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+
+    private String[] mTitles = {"首页", "热评", "我的"};
+    private int[] mIconUnselectIds = {R.mipmap.tab_home_unselect,
+            R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[] mIconSelectIds = {R.mipmap.tab_home_select,
+            R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initData();
     }
+
+    private void initData() {
+        for (String title : mTitles) {
+            mFragments.add(new MainFragment());
+            mFragments.add(new HotCommentFragment());
+            mFragments.add(new MyFragment());
+        }
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+        mTabLayout.setTabData(mTabEntities, this, R.id.container, mFragments);
+        mTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+        mTabLayout.setCurrentTab(1);
+    }
+
 }
---
+++ MaoYanApp/src/main/java/com/mao/movie/fragment/HotCommentFragment.java
@@ -0,0 +1,26 @@
+package com.mao.movie.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mao.movie.R;
+
+/**
+ * 热评Fragment
+ */
+public class HotCommentFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, null);
+        return v;
+    }
+}
