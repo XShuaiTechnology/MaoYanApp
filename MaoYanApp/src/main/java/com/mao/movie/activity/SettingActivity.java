ae650e48dc45f7d5e96ee2b10dcec8744ff49ba7
GaoMatrix <gcquan08@gmail.com>
2016-10-25 19:15:26 +0800

    SettingActivity的布局

---
+++ MaoYanApp/src/main/java/com/mao/movie/activity/SettingActivity.java
@@ -0,0 +1,18 @@
+package com.mao.movie.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mao.movie.R;
+
+/**
+ * Created by GaoMatrix on 2016/10/25.
+ */
+public class SettingActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
--- MaoYanApp/src/main/java/com/mao/movie/fragment/UserFragment.java
+++ MaoYanApp/src/main/java/com/mao/movie/fragment/UserFragment.java
@@ -1,17 +1,39 @@
 package com.mao.movie.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.mao.movie.R;
+import com.mao.movie.activity.SettingActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 我的Fragment
  */
 public class UserFragment extends Fragment {
+
+    @BindView(R.id.userIconTextView)
+    ImageView mUserIconTextView;
+    @BindView(R.id.loginButton)
+    Button mLoginButton;
+    @BindView(R.id.favoriteLayout)
+    RelativeLayout mFavoriteLayout;
+    @BindView(R.id.historyLayout)
+    RelativeLayout mHistoryLayout;
+    @BindView(R.id.settingLayout)
+    RelativeLayout mSettingLayout;
+    @BindView(R.id.feedbackLayout)
+    RelativeLayout mFeedbackLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +43,27 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_user, null);
+        ButterKnife.bind(this, v);
         return v;
+    }
+
+    @OnClick({R.id.userIconTextView, R.id.loginButton, R.id.favoriteLayout, R.id.historyLayout, R.id.settingLayout, R.id.feedbackLayout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.userIconTextView:
+                break;
+            case R.id.loginButton:
+                break;
+            case R.id.favoriteLayout:
+                break;
+            case R.id.historyLayout:
+                break;
+            case R.id.settingLayout:
+                Intent intent = new Intent(getActivity(), SettingActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.feedbackLayout:
+                break;
+        }
     }
 }
