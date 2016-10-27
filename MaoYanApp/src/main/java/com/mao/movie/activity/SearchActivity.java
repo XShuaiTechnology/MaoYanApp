b97bb4d89f22623d08f43a21356900e7a4e72963
GaoMatrix <gcquan08@gmail.com>
2016-10-27 11:28:52 +0800

    添加搜索界面框架

--- MaoYanApp/src/main/java/com/mao/movie/activity/SearchActivity.java
+++ MaoYanApp/src/main/java/com/mao/movie/activity/SearchActivity.java
@@ -1,17 +1,71 @@
 package com.mao.movie.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.mao.movie.R;
+import com.mao.movie.fragment.SearchFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by GaoMatrix on 2016/10/26.
  */
-public class SearchActivity extends BaseActivity {
+public class SearchActivity extends AppCompatActivity {
+    @BindView(R.id.backButton)
+    ImageButton mBackButton;
+    @BindView(R.id.searchEditText)
+    EditText mSearchEditText;
+    @BindView(R.id.searchLineView)
+    View mSearchLineView;
+    @BindView(R.id.deleteSearchImageButton)
+    ImageButton mDeleteSearchImageButton;
+    @BindView(R.id.searchImageButton)
+    ImageButton mSearchImageButton;
+    @BindView(R.id.searchFrameLayout)
+    FrameLayout mSearchFrameLayout;
+
+    private FragmentTransaction mFragmentTransaction;
+    private FragmentManager mFragmentManager;
+    private SearchFragment mSearchFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        showFragmnet(1);
+    }
+
+    private void showFragmnet(int type) {
+        if (type == 1) {
+            mSearchFragment = new SearchFragment();
+            mFragmentTransaction.replace(R.id.searchFrameLayout, mSearchFragment, "SearchFragmnet");
+        }
+        mFragmentTransaction.commitAllowingStateLoss();
+    }
+
+    @OnClick({R.id.backButton, R.id.deleteSearchImageButton, R.id.searchImageButton})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.backButton:
+                break;
+            case R.id.deleteSearchImageButton:
+                break;
+            case R.id.searchImageButton:
+                break;
+        }
     }
 }
---
+++ MaoYanApp/src/main/java/com/mao/movie/fragment/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.mao.movie.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mao.movie.R;
+
+/**
+ * Created by GaoMatrix on 2016/10/27.
+ */
+public class SearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+    }
+
+}
